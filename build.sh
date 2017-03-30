cc4d67e3977ca4a4d380ba8740592fced91c3bca
Martin Peters <martin.peters4@uni-rostock.de>
2017-03-30 14:41:03 +0200

    added main automation script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# check if docker is ok
+docker info &> /dev/null || (echo "Please install docker and ensure it's running and operational" && exit 1)
+
+# build the build-env image
+docker build -f Dockerfile.build-env -t masymos-build-env:latest .
+
+# run the build
+docker run --rm -t -v ./src:/root/src -e "SRC_DIR=/root/src"  masymos-build-env:latest
+
+# build the actual masymos container
+#docker build -f Dockerfile -t freakybytes/masymos:latest .
+
+
