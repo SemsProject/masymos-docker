374e3ca8279e3009d33bc36f170a7589d0fdc078
Martin Peters <martin.peters4@uni-rostock.de>
2017-03-30 16:10:44 +0200

    fixed docker run command

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ docker info &> /dev/null || (echo "Please install docker and ensure it's running
 docker build -f Dockerfile.build-env -t masymos-build-env:latest .
 
 # run the build
-docker run --rm -t -v ./src:/root/src -e "SRC_DIR=/root/src"  masymos-build-env:latest
+docker run --rm -t -v $(pwd)./src/:/root/src/ -e "SRC_DIR=/root/src"  masymos-build-env:latest
 
 # build the actual masymos container
 #docker build -f Dockerfile -t freakybytes/masymos:latest .
